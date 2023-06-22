1a185ac33175028b54ace3a15288fd04e832a83f
Celso Benedetti <celsobenedetti2@gmail.com>
2023-06-22 19:23:47 -0300

    🚀 feat: infinite scroll search endpoint

--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import {
   type AggregationsAggregate,
-  type SearchResponse,
+  type SearchResponse as ClientSearchResponse,
 } from "@elastic/elasticsearch/lib/api/types";
 
 interface Document {
@@ -14,21 +14,54 @@ interface Document {
   dt_creation: Date;
 }
 
-interface Response
-  extends SearchResponse<Document, Record<string, AggregationsAggregate>> {
+type DocumentResponse = ClientSearchResponse<
+  Document,
+  Record<string, AggregationsAggregate>
+>;
+
+interface SearchResponse extends DocumentResponse {
   elapsedTime?: number;
+}
+
+interface InfiniteSearchResponse extends SearchResponse {
+  nextCursor: number;
 }
 
 export const searchRouter = createTRPCRouter({
   search: publicProcedure
     .input(z.object({ query: z.string() }))
-    .query<Response>(async ({ ctx, input }) => {
+    .query<SearchResponse>(async ({ ctx, input }) => {
       const results = await ctx.elastic.search<Document>({
         index: "wikipedia",
+        size: SIZE,
         query: {
           match: { content: input.query },
         },
       });
       return results;
     }),
+
+  infiniteSearch: publicProcedure
+    .input(
+      z.object({
+        query: z.string(),
+        cursor: z.number().nullish(), // <-- "cursor" needs to exist, but can be any type
+      })
+    )
+    .query<InfiniteSearchResponse>(async ({ ctx, input }) => {
+      const cursor = input.cursor ?? 1;
+
+      const results = await ctx.elastic.search<Document>({
+        index: "wikipedia",
+        size: SIZE,
+        from: SIZE * cursor,
+        query: {
+          match: { content: input.query },
+        },
+      });
+
+      return { ...results, nextCursor: cursor + 1 };
+    }),
 });
+
+const SIZE = 10;
