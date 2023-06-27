8102f2b568375c1b37859d1ce4a7eab37e8f765f
Celso Benedetti <celsobenedetti2@gmail.com>
2023-06-27 17:02:30 -0300

    🗑️ remove: exampleRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "@/server/api/routers/example";
 import { createTRPCRouter } from "@/server/api/trpc";
 import { searchRouter } from "./routers/search";
 
@@ -8,7 +7,6 @@ import { searchRouter } from "./routers/search";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   elastic: searchRouter,
 });
 
