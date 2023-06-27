e5a750bda1c0495a395fbc8af7c2b0206b9e2a84
Celso Benedetti <celsobenedetti2@gmail.com>
2023-06-26 22:09:44 -0300

    ➕ add: T3 logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
 import { useSearch } from "@/store/search";
+import Image from "next/image";
 
 export default function Header() {
   const router = useRouter();
@@ -17,21 +18,33 @@ export default function Header() {
       className={`fixed top-0 z-40 flex h-header w-full items-center justify-between gap-12
         bg-background px-6 ${!isHome ? "drop-shadow-sm" : ""}`}
     >
-      {!isHome && (
+      {!isHome ? (
         <button
           onClick={() => {
             router.push("/").catch(console.error);
           }}
+          className="transition-all duration-200 ease-in-out hover:scale-105 hover:transform hover:opacity-80"
         >
-          <HeroIcon shape="home" className="h-10 w-10" />
+          <Image
+            alt="T3 Logo"
+            src={theme == "light" ? "/t3-dark.svg" : "/t3-light.svg"}
+            width={50}
+            height={50}
+          />
         </button>
+      ) : (
+        <div></div>
       )}
       <div className="w-full">
         <div className="flex w-full sm:w-3/5">{!isHome && <Search />}</div>
       </div>
-      <Button onClick={toggleTheme} variant="ghost" className="h-10 w-10">
+      <Button
+        onClick={toggleTheme}
+        variant="ghost"
+        className="h-8 w-8 transition-all duration-200 ease-in-out hover:scale-105 hover:transform hover:opacity-80"
+      >
         <HeroIcon
-          className="absolute h-8 w-8"
+          className="absolute mx-auto my-auto h-8 w-8"
           shape={theme == "light" ? "sun" : "moon"}
         />
       </Button>
