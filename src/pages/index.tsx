aaf25651558f8f6eb9ce2552b902daa9e3f1b265
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-04 21:17:46 -0300

    🎨 style: home page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <main className="bg flex h-screen flex-col items-center justify-center pt-header">
         <h1 className="mb-20 text-7xl font-extrabold tracking-tight sm:text-[5rem]">
-          Elastic<span className="text-indigo-500">T3</span>Wiki
+          <span className="text-indigo-500">T3</span> ElasticWiki
         </h1>
 
         <section className="flex w-10/12 max-w-sm flex-col items-center gap-6 sm:max-w-[36rem]">
