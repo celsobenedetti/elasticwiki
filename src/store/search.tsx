b28be0031647a8cc3e434cf1b5a3ad3f9fa697cd
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-08 10:17:43 -0300

    🧹 chore: remove unused results store

--- src/store/search.tsx
+++ src/store/search.tsx
@@ -1,18 +1,11 @@
-import { type RouterOutputs } from "@/lib/api";
 import { create } from "zustand";
 
-type SearchOutput = RouterOutputs["elastic"]["infiniteSearch"] | undefined;
-
 interface SearchStore {
-  results: SearchOutput;
-  setResults: (results: SearchOutput) => void;
   searchQuery: string;
   setSearchQuery: (query: string) => void;
 }
 
 export const useSearchBar = create<SearchStore>((set) => ({
-  results: undefined,
-  setResults: (results: SearchOutput) => set({ results }),
   searchQuery: "",
   setSearchQuery: (query: string) => {
     if (query) set({ searchQuery: query });
