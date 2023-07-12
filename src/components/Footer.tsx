16f0a6df4be6f32f2d636e4bcf67dc2e1a9e093b
Celso Benedetti <celsobenedetti2@gmail.com>
2023-07-12 19:55:21 -0300

    🧹 chore: stroke-width warning

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,6 @@ function GitHubIcon() {
       <svg
         stroke="currentColor"
         fill="currentColor"
-        stroke-width="0"
         viewBox="0 0 1024 1024"
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5 cursor-pointer text-slate-300 hover:text-black dark:text-active dark:hover:text-slate-300"
