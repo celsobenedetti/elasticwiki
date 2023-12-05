806f7d0acf9a0fe454c3ec284a51cd3fb2687dbc
Celso Benedetti <celsobenedetti2@gmail.com>
2023-12-05 19:19:14 -0300

    terraform: use terraform_data provisioner
    - on newer Terraform version got cyclic dependency when a resource
      provisioner depends on information about the resource on

--- terraform/create-linode.tf
+++ terraform/create-linode.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     linode = {
       source  = "linode/linode"
-      version = "1.27.1"
+      version = "2.5.2"
     }
   }
 }
@@ -13,19 +13,21 @@ provider "linode" {
   token = var.linode_tk
 }
 
-resource "linode_instance" "t3-elasticsearch" {
+resource "linode_instance" "elasticwiki-elasticsearch" {
   image           = "linode/ubuntu18.04"
-  label           = "t3-elasticsearch"
+  label           = "elasticwiki-elasticsearch"
   group           = "Terraform"
   region          = "us-west"
   type            = "g6-standard-2" # 4GB RAM required for elastic-cluster
   root_pass       = var.linode_pass
   authorized_keys = [var.ssh_key]
+}
 
+resource "terraform_data" "provisioner" {
   connection {
     type = "ssh"
     user = var.linode_user
-    host = tolist(linode_instance.t3-elasticsearch.ipv4)[0]
+    host = tolist(linode_instance.elasticwiki-elasticsearch.ipv4)[0]
   }
 
   provisioner "remote-exec" {
@@ -36,25 +38,26 @@ resource "linode_instance" "t3-elasticsearch" {
       "systemctl enable docker",
       "systemctl start docker",
       "sysctl -w vm.max_map_count=262144",
+      "mkdir ${var.elastic_dir}",
     ]
   }
 
   provisioner "file" {
     source      = "../elasticsearch/elastic-cluster.yml"
-    destination = "/${var.elastic_dir}/docker-compose.yml"
+    destination = "${var.elastic_dir}/docker-compose.yml"
   }
 
   provisioner "file" {
     source      = "../.env"
-    destination = "/${var.elastic_dir}/.env"
+    destination = "${var.elastic_dir}/.env"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "cd /${var.elastic_dir}",
+      "cd ${var.elastic_dir}",
 
       # run elasticsearch cluster
-      "export ELASTIC_HOST_IP=${tolist(linode_instance.t3-elasticsearch.ipv4)[0]} && docker-compose up -d",
+      "export ELASTIC_HOST_IP=${tolist(linode_instance.elasticwiki-elasticsearch.ipv4)[0]} && docker-compose up -d",
 
       # copy certificate from inside the container to VM FS
       "docker cp $(docker ps | grep es01 | awk '{print $1}'):/usr/share/elasticsearch/config/certs/ca/ca.crt .",
@@ -63,7 +66,7 @@ resource "linode_instance" "t3-elasticsearch" {
 
   # copy certificate from VM to local FS
   provisioner "local-exec" {
-    command = "scp -o StrictHostKeyChecking=no -r ${var.linode_user}@${tolist(linode_instance.t3-elasticsearch.ipv4)[0]}:/${var.elastic_dir}/ca.crt ."
+    command = "scp -o StrictHostKeyChecking=no -r ${var.linode_user}@${tolist(linode_instance.elasticwiki-elasticsearch.ipv4)[0]}:/${var.elastic_dir}/ca.crt ."
   }
 
   # build elasticsearch index
@@ -71,7 +74,7 @@ resource "linode_instance" "t3-elasticsearch" {
     command = <<EOT
     python3 ../elasticsearch/build_index.py \
             --cert ca.crt \
-            --host https://${tolist(linode_instance.t3-elasticsearch.ipv4)[0]}:${var.elastic_port} \
+            --host https://${tolist(linode_instance.elasticwiki-elasticsearch.ipv4)[0]}:${var.elastic_port} \
             --es-dir "../elasticsearch" \
     EOT
   }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,4 @@
-#
 output "linode_ip" {
   description = "New Linode instance ip address"
-  value       = linode_instance.t3-elasticsearch.ipv4
+  value       = linode_instance.elasticwiki-elasticsearch.ipv4
 }
